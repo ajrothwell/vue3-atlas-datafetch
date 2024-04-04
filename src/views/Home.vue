0bf3da6c5bfa1f792972f948b44bae1fd3ddc37a
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-04-04 17:39:55 -0400

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+import { ref } from 'vue';
+
+const address = ref('');
+
+const handleSearch = () => {
+  console.log('searching for:', address.value);
+}
+
+
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <div class="columns">
+        <input
+          class="input column is-6"
+          type="text"
+          placeholder="Search an address"
+          v-model="address"
+        />
+        <button
+          class="button"
+          @click="handleSearch"
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  </main>
+</template>
