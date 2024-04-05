7e2cb876a3d944295b2fb9c21655d01d14dbfbb4
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-04-05 17:10:30 -0400

    cleanup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
+console.log('Home setup');//, route:', route);
 
 import { useRouter } from 'vue-router';
 const router = useRouter();
-
-console.log('Home setup');//, route:', route);
 
 import { ref, computed } from 'vue';
 
@@ -11,7 +10,9 @@ const address = ref('');
 
 const handleSearch = () => {
   console.log('searching for:', address.value);
-  router.push({ name: 'address', params: { address: address.value } });
+  if (address.value) {
+    router.push({ name: 'address', params: { address: address.value } });
+  }
 }
 
 </script>
@@ -25,7 +26,6 @@ const handleSearch = () => {
           type="text"
           placeholder="Search an address"
           v-model="address"
-          @submit="handleSearch"
         />
         <!-- I could not use a router-link here because the address is null at the start,
           and it would throw an error for a missing param
